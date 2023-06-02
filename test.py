3d1500b2fff8b34d95888920c68406e194697a1f
OlegShir <63945818+OlegShir@users.noreply.github.com>
2023-06-02 16:45:45 +0300

    source

--- test.py
+++ test.py
@@ -1,13 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open("C:/Users/X/Desktop/185900/test.adc", 'rb') as rgg_file:
-        new_part_rgg_file = np.frombuffer(rgg_file.read(1000000), dtype='int64')
-        # создание копии из буфера для возможности его редактивования
+N1 = 4000
+LCHM = np.zeros((N1, 1))
+for n in range(N1):
+        LCHM[n,0] = np.pi * 300e6 * n**2 / (N1 * 400e6) - np.pi * 300e6 * (n) / 400e6
+Tpi = 485.692e-6
 
-        # plot
-        fig, ax = plt.subplots()
+dx = 108*Tpi # Шаг по азимуту
+Qx:float = 0.0351/0.23 # ширина ДНА по азимутуD
+QRm:float = Qx*4180 #  мгновенно облучаемый участок на дальности R, [м] 
+QR:int = int(np.fix(QRm/dx/2)*2) # мгновенно облучаемый участок на дальности R, [отсчет]
 
-        ax.plot(new_part_rgg_file)
+# Создание массива индексов от 0 до QR-1
+indices = np.arange(QR)
 
-        plt.show()
+# Вычисление массива x без использования цикла
+x = (-QR / 2 + indices) * dx
+
+rt = np.sqrt(4180 ** 2 + x ** 2)
+
+
+
+x += 0
